538eba602fe1b433d2d7343958e1a6e476c75783
tooth_N_tail <10995361+tooth-N-tail@users.noreply.github.com>
2019-12-20 11:03:39 +0000

    Update fresh-debian-setup.sh

--- fresh-debian-setup.sh
+++ fresh-debian-setup.sh
@@ -4,7 +4,7 @@
 # Personalise a fresh Debian install #
 ######################################
 
-# To execute the script, run the below command. Taken from https://askubuntu.com/a/992451 
+# To execute the script, run the below command. Taken from - https://askubuntu.com/a/992451 
 # -O -  Allows us to output to nowhere and into the bash pipe.
 # wget -O - https://raw.githubusercontent.com/tooth-N-tail/Snippets/master/fresh-debian-setup.sh | sudo bash
 
@@ -28,3 +28,42 @@ apt update -qq && apt upgrade -qq && apt autoremove -qq
 # -O Output file.
 rm ~/.bash_aliases
 wget -q https://raw.githubusercontent.com/tooth-N-tail/Snippets/master/bash_aliases -O ~/.bash_aliases
+
+# Add sudo user and grant privileges
+useradd --create-home --shell "/bin/bash" --groups sudo jeff
+
+# Create SSH 
+# Guide from - https://www.digitalocean.com/community/tutorials/automating-initial-server-setup-with-ubuntu-18-04
+
+
+# Install core utilites
+# Script from - https://unix.stackexchange.com/a/434061
+CORE_PROGRAMS=(git python3 python3-pip curl)
+
+for PROGRAM in "${CORE_PROGRAMS[@]}"; do
+    if ! command -v "$CORE_PROGRAMS" > /dev/null 2>&1; then
+        apt-get install "$CORE_PROGRAMS" -qq
+    fi
+done
+
+# setup go
+# $(..) is command Substitution and is equivalent to `..`. Basically menaing to execute the command within. See "man bash".
+ORIGINAL_GO=$(which go)
+rm $ORIGINAL_GO
+cd ~
+VERSION=$(curl https://golang.org/VERSION?m=text) # Returns in form of "go1.13.5"
+wget https://dl.google.com/go/$VERSION.linux-amd64.tar.gz
+tar -C /usr/local -xzf $VERSION.linux-amd64.tar.gz
+echo "export GOPATH=~/go" >> ~/.profile # source intentionally not used here as it appears on next line.
+echo "export PATH='$PATH':/usr/local/go/bin:$GOPATH/bin" >> ~/.profile && source ~/.profile
+rm $VERSION.linux-amd64.tar.gz
+
+# Install everything else
+# Script from - https://unix.stackexchange.com/a/434061
+PROGRAMS=(git python3 python3-pip curl)
+
+for PROGRAM in "${PROGRAMS[@]}"; do
+    if ! command -v "$PROGRAMS" > /dev/null 2>&1; then
+        apt-get install "$PROGRAMS" -qq
+    fi
+done

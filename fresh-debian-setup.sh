8a0da9bea19ddde78c31fed169a6ba6ed080eea1
tooth_N_tail <10995361+tooth-N-tail@users.noreply.github.com>
2020-01-04 17:47:18 +0000

    Update fresh-debian-setup.sh

--- fresh-debian-setup.sh
+++ fresh-debian-setup.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 
-######################################
-# Personalise a fresh Debian install #
-######################################
+############################################
+### FRESH DEBIAN INSTALL PERSONALISATION ###
+### VERSION 0.1                          ###
+############################################
 
-# To execute the script, run the below command. Taken from - https://askubuntu.com/a/992451 
-# -O -  Allows us to output to nowhere and into the bash pipe.
-# wget -O - https://raw.githubusercontent.com/tooth-N-tail/Snippets/master/fresh-debian-setup.sh | sudo bash
+### Intial script idea and some techniques taken from:
+### https://www.digitalocean.com/community/tutorials/automating-initial-server-setup-with-ubuntu-18-04
+### https://dotnetrussell.com/SetupBare.sh
+### https://unix.stackexchange.com/a/434061
+###
+### To execute the script, run the below command. Taken from - https://askubuntu.com/a/992451 
+### -O -  Allows us to output to nowhere and into the bash pipe.
+### wget -O - https://raw.githubusercontent.com/tooth-N-tail/Snippets/master/fresh-debian-setup.sh | sudo bash
 
-# This allows us to keep the script tidy.
+### This allows us to keep the script tidy ###
 # -e exits as soon as any line in the bash script fails.
 # -x prints each command that is going to be executed.
 set -ex
 
-# Before we proceed check that we have a connection to the scary outside world.
+### Before we proceed check that we have a connection to the scary outside world ###
 # -q Quiet.
 # -c Number of pings to perform.
-# $? Returns the exit status of the command previously executed. If ping is successful, $? will return 0. If not, it will return another number.
+# Returns the exit status of the command previously executed. If ping is successful then it will return 0. If not, it will return another number.
 ping -q -c1 google.co.uk > /dev/null
 
-# Provide a username to use as the main user.
-USERNAME='jeff'
+### Name of the user to create and grant sudo privileges ###
+USERNAME=jeff
 
-# Change the frontend default behavior of debconf to noninteractive.
+### Change the frontend default behavior of debconf to noninteractive ###
 # This helps to make the installs and updates etc non-interactive. (i.e You don't get asked questions)
 # Otherwise you have to run something like this each time as the noninteractive will not persist: sudo DEBIAN_FRONTEND=noninteractive apt-get install slrn.
 dpkg-reconfigure debconf --frontend=noninteractive
 
-# Update, upgrade and clean up.
+### Update, upgrade and clean up ###
 # -qq should imply -y but doesn't work so using -qy here.
 apt update -qy && apt upgrade -qy && apt autoremove -qy
 
-# Remove ~/.bash_aliases and recreate from GitHub file. These are personalised bash commands.
+### Remove ~/.bash_aliases and recreate from GitHub file. These are personalised bash commands. ###
 # -q Quiet.
 # -O Output file.
 rm ~/.bash_aliases
 wget -q https://raw.githubusercontent.com/tooth-N-tail/Snippets/master/bash_aliases -O ~/.bash_aliases
 
-# Add sudo user and grant privileges
-useradd --create-home --shell "/bin/bash" --groups sudo $USERNAME
+### Add sudo user and grant privileges ###
+useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
 
-# Create SSH 
+### SETUP SSH ###
 # Guide from - https://www.digitalocean.com/community/tutorials/automating-initial-server-setup-with-ubuntu-18-04
 
 
-# Install core utilities. The for loop will check if the application exists before attempting an install.
+### Install core utilities. The for loop will check if the application exists before attempting an install ###
 # Script from - https://unix.stackexchange.com/a/434061
 CORE_PROGRAMS=(git python3 python3-pip curl)
 
@@ -54,7 +60,7 @@ for PROGRAM in "${CORE_PROGRAMS[@]}"; do
     fi
 done
 
-# Setup & install golang.
+### Setup & install golang ###
 # $(..) is command Substitution and is equivalent to `..`. Basically menaing to execute the command within. See "man bash".
 ORIGINAL_GO=$(which go)
 rm $ORIGINAL_GO
@@ -66,7 +72,7 @@ echo "export GOPATH=~/go" >> ~/.profile # source intentionally not used here as 
 echo "export PATH='$PATH':/usr/local/go/bin:$GOPATH/bin" >> ~/.profile && source ~/.profile
 rm $VERSION.linux-amd64.tar.gz
 
-# Install everything else
+### Install everything else ###
 # Script from - https://unix.stackexchange.com/a/434061
 PROGRAMS=(git python3 python3-pip curl)
 
@@ -76,5 +82,5 @@ for PROGRAM in "${PROGRAMS[@]}"; do
     fi
 done
 
-# Reminders and advisories
+### Reminders and advisories ###
 echo "As root run 'passwd $USERNAME' to set the password. Currently it is blank and insecure."

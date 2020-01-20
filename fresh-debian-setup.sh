50f52c63312ef88085f52643893b819ad3001c99
JeffreyShran <10995361+JeffreyShran@users.noreply.github.com>
2020-01-20 13:02:14 +0000

    -

--- fresh-debian-setup.sh
+++ fresh-debian-setup.sh
@@ -15,7 +15,7 @@
 #
 # To execute the script, run the below command.
 # Taken from - https://askubuntu.com/a/992451. "-O -" Allows us to output to nowhere and into the bash pipe. Frequent runs cause caching so added date var.
-#    wget -O - "https://raw.githubusercontent.com/JeffreyShran/Snippets/master/fresh-debian-setup.sh?$(date +%s)" | sudo bash
+#    wget --no-cache -O - "https://raw.githubusercontent.com/JeffreyShran/Snippets/master/fresh-debian-setup.sh?$(date +%s)" | bash
 #---------------------------------------------------------------------------------------------------------------------------------------------------
 
 # This helps us to keep the script tidy in respect to error handling
@@ -70,31 +70,33 @@ function version() { # https://apple.stackexchange.com/a/123408 - You need to de
 
 AVAILABLEVERSION=$(curl -s https://golang.org/VERSION?m=text) # Returns in form of "go1.13.5"
 
-if [[ $(which go) ]]; then # https://stackoverflow.com/a/35165216/4373967
-  echo "Found golang installation"
-  INSTALLEDVERSION=$(go version | {
-    read _ _ v _
-    echo ${v#go}
-  }) # Strips out the response and returns in the form of "1.13.5"
-else
-  echo "Installing golang from source as no current version exists"
-  # Pulls down latest golang direct from Google and sets PATH / GOPATH
+function installGoFromTheGOOG() { # Pulls down latest golang direct from Google and sets PATH / GOPATH
   cd ~
   wget https://dl.google.com/go/$AVAILABLEVERSION.linux-amd64.tar.gz
   tar -C /usr/local -xzf $AVAILABLEVERSION.linux-amd64.tar.gz
   echo "export GOPATH=~/go" >>~/.profile # source intentionally not used here as it appears on next line
   echo "export PATH='$PATH':/usr/local/go/bin:$GOPATH/bin" >>~/.profile && source ~/.profile
   rm $AVAILABLEVERSION.linux-amd64.tar.gz
+}
+
+if [[ $(which go | tr -d ' \n\r\t ' | head -c1 | wc -c) -ne 0 ]]; then # https://stackoverflow.com/a/35165216/4373967
+  echo "Found golang installation"
+  INSTALLEDVERSION=$(go version | {
+    read _ _ v _
+    echo ${v#go}
+  }) # Strips out the response and returns in the form of "1.13.5"
+
+  if [ $(version $INSTALLEDVERSION | cut -c 3-) -lt $(version $AVAILABLEVERSION) ]; then # Comparison Operators - http://tldp.org/LDP/abs/html/comparison-ops.html also pipe to cut and remove leading 2 characters
+    echo "Current go version is older than the one available from Google"
+    rm -f $(which go)    # remove current golang if exists. -f will ignore nonexistent files, never prompt
+    installGoFromTheGOOG # Update to latest verion
+  else
+    echo "Currently installed golang v$INSTALLEDVERSION is already latest version"
+  fi
+else
+  echo "Installing golang from source as no current version exists"
+  installGoFromTheGOOG
 fi
-
-#if [ $(version $INSTALLEDVERSION | cut -c 3-) -lt $(version $AVAILABLEVERSION) ]; then # Comparison Operators - http://tldp.org/LDP/abs/html/comparison-ops.html also pipe to cut and remove leading 2 characters
-#  echo "Current go version is older than the one available from Google"
-#  rm -f $(which go)    # remove current golang if exists. -f will ignore nonexistent files, never prompt
-#  installGoFromTheGOOG # Update to latest verion
-#else
-#  echo "Currently installed golang v$INSTALLEDVERSION is already latest version"
-#fi
-
 cat <<"EOF"
                 ___
             ,-'"   "`-.

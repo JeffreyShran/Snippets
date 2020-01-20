701871fdfcbeec7f61024db63e09fd14f52f3f6e
JeffreyShran <10995361+JeffreyShran@users.noreply.github.com>
2020-01-20 11:38:36 +0000

    tidied

--- fresh-debian-setup.sh
+++ fresh-debian-setup.sh
@@ -63,9 +63,17 @@ if ! dpkg -s $pkgs >/dev/null 2>&1; then # Script from - https://stackoverflow.c
 fi
 
 # Setup & install golang
+INSTALLEDVERSION=$(go version | {
+    read _ _ v _
+    echo ${v#go}
+  }) # Strips out the response and returns in the form of "1.13.5"
+
+AVAILABLEVERSION=$(curl -s https://golang.org/VERSION?m=text) # Returns in form of "go1.13.5"
+
 function version() { # https://apple.stackexchange.com/a/123408 - You need to define functions in advance of you calling them in your script
 echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
 }
+
 function installGoFromTheGOOG() { # Pulls down latest golang direct from Google and sets PATH / GOPATH
     cd ~
     wget https://dl.google.com/go/$VERSION.linux-amd64.tar.gz
@@ -76,11 +84,7 @@ function installGoFromTheGOOG() { # Pulls down latest golang direct from Google 
 }
 
 if [[ $(which go) ]]; then # $(..) is command Substitution and is equivalent to `..`. Basically meaning to execute the command within. See "man bash"
-  INSTALLEDVERSION=$(go version | {
-    read _ _ v _
-    echo ${v#go}
-  }) # Strips out the response and returns in the form of "1.13.5"
-  AVAILABLEVERSION=$(curl -s https://golang.org/VERSION?m=text) # Returns in form of "go1.13.5"
+
   if [ $(version $INSTALLEDVERSION | cut -c 3-) -lt $(version $AVAILABLEVERSION) ]; then # Comparison Operators - http://tldp.org/LDP/abs/html/comparison-ops.html also pipe to cut and remove leading 2 characters
     rm -f $(which go) # remove current golang if exists. -f will ignore nonexistent files, never prompt
     installGoFromTheGOOG # Update to latest verion

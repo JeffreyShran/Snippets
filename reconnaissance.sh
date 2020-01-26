2fd7b6c27e098361721804f92142de60f849675f
jeff <10995361+tooth-N-tail@users.noreply.github.com>
2020-01-26 14:01:58 +0000

    -

--- reconnaissance.sh
+++ reconnaissance.sh
@@ -4,11 +4,14 @@ set -o errexit # added || true to commands that are allowed to fail.
 
 ((!$#)) && echo No arguments supplied! && exit 1 # Exit in the absence of any arguments
 
+# https://github.com/mazen160/bfac # An automated tool that checks for backup artifacts that may disclose the web-application's source code.
 
-
-
-
-
+# Amass - Install from kali repository
+# Project Sonar - AWS, then build autonomous method in future
+# commonspeak2 - Python script, later rewrite in golang
+# waybackurls - go get github.com/tomnomnom/waybackurls
+# dnsgen - pip3 install dnsgen
+# httprobe - go get -u github.com/tomnomnom/httprobe
 
 if [ -z ${var+x} ];
 then

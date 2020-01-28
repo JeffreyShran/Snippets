8df176b8181dec95e31378aaefd98e369753ae19
jeff <10995361+tooth-N-tail@users.noreply.github.com>
2020-01-28 10:21:28 +0000

    -

--- reconnaissance.sh
+++ reconnaissance.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-
-set -o errexit # added || true to commands that are allowed to fail.
-
-((!$#)) && echo No arguments supplied! && exit 1 # Exit in the absence of any arguments
 
 # https://github.com/mazen160/bfac # An automated tool that checks for backup artifacts that may disclose the web-application's source code.
 
@@ -13,9 +8,3 @@ set -o errexit # added || true to commands that are allowed to fail.
 # dnsgen - pip3 install dnsgen
 # httprobe - go get -u github.com/tomnomnom/httprobe
 
-if [ -z ${var+x} ];
-then
-    echo "var is unset";
-else
-    echo "var is set to '$var'";
-fi

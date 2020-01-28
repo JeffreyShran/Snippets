3874fc5592c72fbf4fdeef86635eea2c930c3ad0
JeffreyShran <10995361+JeffreyShran@users.noreply.github.com>
2020-01-28 11:00:32 +0000

    -

--- reconnaissance.go
+++ reconnaissance.go
@@ -1,10 +1,21 @@
 
-# https://github.com/mazen160/bfac # An automated tool that checks for backup artifacts that may disclose the web-application's source code.
 
-# Amass - Install from kali repository
-# Project Sonar - AWS, then build autonomous method in future
-# commonspeak2 - Python script, later rewrite in golang
-# waybackurls - go get github.com/tomnomnom/waybackurls
-# dnsgen - pip3 install dnsgen
-# httprobe - go get -u github.com/tomnomnom/httprobe
+// <DOMAIN>
 
+// Amass - Install from kali repository
+	// amass enum --passive -d <DOMAIN>
+// Project Sonar - AWS, then build autonomous method in future
+	// 
+// commonspeak2 - Python script, later rewrite in golang
+	// 
+// waybackurls - go get github.com/tomnomnom/waybackurls
+	//
+// SORT ALL DATA
+// dnsgen - pip3 install dnsgen
+	//
+// httprobe - go get -u github.com/tomnomnom/httprobe
+	//
+// gobuster - go get github.com/OJ/gobuster
+	// gobuster dir -u <DOMAIN> -w ~/wordlists/shortlist.txt -q -n -e
+// bfac - git clone https://github.com/mazen160/bfac.git
+	// bfac --list testing_list.txt (read help)

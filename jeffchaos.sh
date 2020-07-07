0c0771728165c0d7ff40cb9900624ed97616c3cd
jeff <10995361+JeffreyShran@users.noreply.github.com>
2020-07-07 14:54:08 +0100

    updated to be properly formed JSON

--- jeffchaos.sh
+++ jeffchaos.sh
@@ -27,5 +27,5 @@ if [ ${#COMM_DATA[@]} -eq 0 ]; then
     exit 1
 else
     curl -s https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/master/data/hackerone_data.json |
-    jq '.[] | select(.url|inside('${COMM_DATA[@]}')) | {name: .name, url: .url, bounty: .offers_bounties, domains: [.targets.in_scope[] | select(.asset_type=="URL") | .asset_identifier]}'
+    jq '[ .[] | select(.url|inside('${COMM_DATA[@]}')) | {name: .name, url: .url, bounty: .offers_bounties, domains: [.targets.in_scope[] | select(.asset_type=="URL") | .asset_identifier]} ]'
 fi

